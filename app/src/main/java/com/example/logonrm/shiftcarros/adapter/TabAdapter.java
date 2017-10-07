aa5154a61cdb916094f01895c9c8beaeaaf82681
jotace.assis <jotace.assis@outlook.com>
2017-10-07 15:20:08 -0300

    Configuracao das tabs

--- app/src/main/java/com/example/logonrm/shiftcarros/adapter/TabAdapter.java
+++ app/src/main/java/com/example/logonrm/shiftcarros/adapter/TabAdapter.java
@@ -4,7 +4,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import com.example.logonrm.shiftcarros.fragments.PokemonFragment;
+import com.example.logonrm.shiftcarros.fragments.StarWarsFragment;
+
 public class TabAdapter extends FragmentStatePagerAdapter {
+
+    public static int TOTAL_ABAS = 2;
 
     public TabAdapter(FragmentManager fm) {
         super(fm);
@@ -12,11 +17,20 @@ public class TabAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new PokemonFragment();
+                break;
+            case 1:
+                fragment = new StarWarsFragment();
+                break;
+        }
+        return fragment;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return TOTAL_ABAS;
     }
 }
